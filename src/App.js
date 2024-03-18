61da15a6079a6b6973fcb4733526f377ebc1a189
motaz99 <motaz.dn.ali@gmail.com>
2024-03-18 15:11:45 +0300

    feat: initiate the socket & connect it with the backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:8080");
+
 function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      console.log(data);
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+
+    return () => {
+      socket.off("receive_message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    const newMessage = {
+      sender: socket.id,
+      message: message,
+    };
+
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+
+    socket.emit("send_message", { message, client: socket.id });
+
+    setMessage("");
+  };
+
   return (
     <div className="App">
-      <header>Hello react</header>
+      <div>
+        <h1>Chat App</h1>
+        {messages.map((msg, index) => (
+          <div key={index}>
+            <strong>{msg.client || msg.sender}: </strong> {msg.message}
+          </div>
+        ))}
+      </div>
+      <input
+        placeholder="Message..."
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+      />
+      <button onClick={sendMessage}> Send Message</button>
     </div>
   );
 }
